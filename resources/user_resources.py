eafe432ecf81a280d8adbc43198c73e1943489f4
Jordi Mateo Fornés (UdL) <61190134+JordiMateoUdL@users.noreply.github.com>
2020-04-27 16:36:06 +0200

    user_resources

--- resources/user_resources.py
+++ resources/user_resources.py
@@ -6,12 +6,13 @@ import re
 
 import falcon
 from falcon.media.validators import jsonschema
+from sqlalchemy import and_
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 import phonenumbers
 
 import messages
-from db.models import User, Instruments, MusicalGenere, AssociationUserInstruments
+from db.models import User, Instruments, MusicalGenere, AssociationUserInstruments, AssociationUserMusicalGenere
 from hooks import requires_auth
 from resources.base_resources import DAMCoreResource
 from resources.schemas import SchemaRegisterUser
@@ -96,30 +97,43 @@ class ResourceAddInstrument(DAMCoreResource):
 
         current_user = req.context["auth_user"]
 
-        # Init association
-        association = AssociationUserInstruments()
 
         try:
-            if req.media["name"] is not None:
-                aux_instrument_name = req.media["name"]
+            for instrument in req.media:
+                print(instrument)
+                if (instrument["name"] and instrument["expirience"]) is not None:
 
-            # Query to get the instrument data from Instruments table
-            aux_instrument = self.db_session.query(Instruments). \
-                filter(Instruments.name == aux_instrument_name).one()
+                    aux_instrument_name = instrument["name"]
+
+                    aux_instrument = self.db_session.query(Instruments). \
+                        filter(Instruments.name == aux_instrument_name).one()
+
+                    user_exp_instrument = \
+                        self.db_session.query(AssociationUserInstruments).filter(
+                        AssociationUserInstruments.id_user == current_user.id,
+                        AssociationUserInstruments.id_instrument == aux_instrument.id_instrument
+                        ).all()
+
+                    if len(user_exp_instrument) == 0:
+                        # Init association
+                        association = AssociationUserInstruments()
+                        # Inserting the instrument into the relationship
+                        association.assoc_instruments = aux_instrument
+                        association.expirience = instrument["expirience"]
+                        # Finally we save our instrument with his expirience in ot user
+                        current_user.user_instruments.append(association)
+                    else:
+                        print("El usuari tiene ya experiencia, simplemente actualizo")
+                        user_exp_instrument[0].expirience = instrument["expirience"]
+
+
+
+            self.db_session.commit()
+            resp.status = falcon.HTTP_200
+
         except NoResultFound:
             raise falcon.HTTPBadRequest(description=messages.instrument_dont_exist)
 
-        # Set none constrained columns, expirience
-        if req.media["expirience"] is not None:
-            association.expirience = req.media["expirience"]
-
-        # Inserting the instrument into the relationship
-        association.assoc_instruments = aux_instrument
-        # Finally we save our instrument with his expirience in ot user
-        current_user.user_instruments.append(association)
-
-        self.db_session.commit()
-        resp.status = falcon.HTTP_200
 
 
 @falcon.before(requires_auth)
@@ -127,78 +141,70 @@ class ResourceGetTableInstruments(DAMCoreResource):
     def on_get(self, req, resp, *args, **kwargs):
         super(ResourceGetTableInstruments, self).on_get(req, resp, *args, **kwargs)
 
-        # @JORDI: Si em poguesis aconsellar, o donar uns tips, alhora de
-        #         fer una query de la llista de instruments
-        #         Cada ROW tindría = Instrument.name, AssociationUserInstrument.expirience
-        #   m'agradría fer-ho de la millor manera
+        current_user = req.context["auth_user"]
 
+        user_instruments_query = self.db_session.query(AssociationUserInstruments.expirience, Instruments.name).join(
+            Instruments).filter(
+            AssociationUserInstruments.id_user == current_user.id)
 
-        pass
+        response_instruments = list()
+        aux_response = user_instruments_query.all()
+
+        if aux_response is not None:
+            for current_instrument in aux_response:
+                response = {
+                    'expirience': current_instrument[0],
+                    'instrument:': current_instrument[1]
+                }
+                response_instruments.append(response)
+
+        resp.media = response_instruments
+        resp.status = falcon.HTTP_200
 
 
 @falcon.before(requires_auth)
 class ResourceRemoveInstrument(DAMCoreResource):
-    def on_post(self, req, resp, *args, **kwargs):
-        super(ResourceRemoveInstrument, self).on_post(req, resp, *args, **kwargs)
-
-        # @JORDI: Fixat en aquet exemplex,la taula Associació, és un
-        #         objecte Associador, per a permetre l'incorporació de la columna
-        #         experiència.
-        #         - Ja que es un objecte Mapejat a la base de dades, eliminar
-        #           es una feina fácil
+    def on_delete(self, req, resp, *args, **kwargs):
+        super(ResourceRemoveInstrument, self).on_delete(req, resp, *args, **kwargs)
 
         current_user = req.context["auth_user"]
-
-        if "name" in kwargs:
-            query = self.db_session.query(AssociationUserInstruments).join(Instruments)
-            aux_instrument = query.\
-                filter(Instruments.name == kwargs["name"]).one()
-
-            print(aux_instrument.id_instrument)
-
-        self.db_session.delete(aux_instrument)
-        self.db_session.commit()
-        resp.status = falcon.HTTP_200
-
-
-@falcon.before(requires_auth)
-class ResourceAddGenere(DAMCoreResource):
-    def on_post(self, req, resp, *args, **kwargs):
-        super(ResourceAddGenere, self).on_post(req, resp, *args, **kwargs)
-
-        current_user = req.context["auth_user"]
-
-        if "name" in kwargs:
-            aux_musicalgenere = self.db_session.query(MusicalGenere).filter(MusicalGenere.name == kwargs["name"]).one()
-
-        current_user.user_musicalgeneres.append(aux_musicalgenere)
-
-        self.db_session.commit()
-        resp.status = falcon.HTTP_200
+        try:
+            if "name" in kwargs:
+                query = self.db_session.query(AssociationUserInstruments).join(Instruments)
+                aux_instrument = query. \
+                    filter(Instruments.name == kwargs["name"],
+                           AssociationUserInstruments.id_user == current_user.id).one()
+                self.db_session.delete(aux_instrument)
+            self.db_session.commit()
+            resp.status = falcon.HTTP_200
+        except KeyError:
+            raise falcon.HTTPBadRequest(description=messages.parameters_invalid)
+        except NoResultFound:
+            # Aquest missatge es mostra si (instrument no existeix) o (instrument no es del usuari)
+            raise falcon.HTTPBadRequest(description=messages.instrument_dont_exist)
 
 
+#Seguir model instruments
 @falcon.before(requires_auth)
 class ResourceAddGeneres(DAMCoreResource):
     def on_post(self, req, resp, *args, **kwargs):
         super(ResourceAddGeneres, self).on_post(req, resp, *args, **kwargs)
 
-
-    
         current_user = req.context["auth_user"]
 
         if req.media["list_generes"] is not None:
             aux_list = req.media["list_generes"]
 
-            for i in range(len(aux_list)):
-                aux_genere_name = aux_list.pop(i - 1)
-                aux_musicalgenere = self.db_session.query(MusicalGenere). \
-                    filter(MusicalGenere.name == aux_genere_name).one()
-                current_user.user_musicalgeneres.append(aux_musicalgenere)
+            for name in aux_list:
+                aux_musical_genre = self.db_session.query(MusicalGenere). \
+                    filter(MusicalGenere.name == name).one()
+                if aux_musical_genre is not None:
+                    current_user.user_musicalgeneres.append(aux_musical_genre)
 
         self.db_session.commit()
         resp.status = falcon.HTTP_200
 
-
+# Seguir model instrument
 @falcon.before(requires_auth)
 class ResourceGetGenereList(DAMCoreResource):
     def on_get(self, req, resp, *args, **kwargs):
@@ -216,8 +222,8 @@ class ResourceGetGenereList(DAMCoreResource):
 
 @falcon.before(requires_auth)
 class ResourceRemoveGenere(DAMCoreResource):
-    def on_post(self, req, resp, *args, **kwargs):
-        super(ResourceRemoveGenere, self).on_post(req, resp, *args, **kwargs)
+    def on_delete(self, req, resp, *args, **kwargs):
+        super(ResourceRemoveGenere, self).on_delete(req, resp, *args, **kwargs)
 
         # @JORDI: Aquí esta el problema, en aquet cas no es un objecte mapejat
         #         llavors s'hauría de esborrar la relació entre User y Instruments
@@ -227,15 +233,16 @@ class ResourceRemoveGenere(DAMCoreResource):
         current_user = req.context["auth_user"]
 
         if "name" in kwargs:
-            if __name__ == '__main__':
 
-                # busquem el genere introduit per a esborrar
+            musical_genere = self.db_session.query(MusicalGenere) \
+                .filter(MusicalGenere.name == kwargs["name"]).one()
 
-                aux_genere = self.db_session.query(MusicalGenere). \
-                                filter(MusicalGenere.name == kwargs["name"]).one()
+            d = AssociationUserMusicalGenere.delete().where(and_(
+                AssociationUserMusicalGenere.c.id_user == current_user.id,
+                AssociationUserMusicalGenere.c.id_genere == musical_genere.id
+            ))
 
+            self.db_session.execute(d)
 
         self.db_session.commit()
         resp.status = falcon.HTTP_200
-
-
